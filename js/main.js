59f5ccc20e37389868600442ac08795ceba960ef
SergioScata <sergiopio395@gmail.com>
2023-11-09 16:07:25 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Richiedo all'utente le informazioni che mi servono
 
 let name= prompt ("Inserisci il tuo nome");
-let surname= prompt("Inserisci il tuo cogonome");
+let surname= prompt("Inserisci il tuo cognome");
 let favouritecolor= prompt("Inserisci il tuo colore preferito")
 let password=name+surname+favouritecolor;
 
