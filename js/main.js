6ebfe68de2971928f37d9e5690c150ff28730537
SergioScata <sergiopio395@gmail.com>
2023-11-09 14:40:27 +0100

    Started project

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Questo è il file che contiene tutto il codice JS della pagina web
+
+let name= prompt ("Inserisci il tuo nome");
+let surname= prompt("Inserisci il tuo cogonome");
+let favouritecolor= prompt("Inserisci il tuo colore preferito")
